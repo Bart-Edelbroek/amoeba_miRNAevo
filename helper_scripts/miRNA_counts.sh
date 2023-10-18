27f4d10487b6b7be7173a07b517b92b0d78fa215
Bart-Edelbroek <62546595+Bart-Edelbroek@users.noreply.github.com>
2023-10-18 14:21:57 +0200

    Add files via upload

--- helper_scripts/miRNA_counts.sh
+++ helper_scripts/miRNA_counts.sh
@@ -1,1 +1,2 @@
-featureCounts -a ../data_in/ddis/sRNAs_ddis_mature.gff -T 8 -O -M --fraction --fracOverlap 1  --fracOverlapFeature 1 -t miRNA -g Name -o ../data_in/miRNA_counts.txt mapped/* 
+featureCounts -a ../data_in/ddis/sRNAs_ddis_mature.gff -T 8 -O -M --fraction --fracOverlap 1  --fracOverlapFeature 0.9 -t miRNA-5p -g Name -o ../data_in/miRNA_5p_counts.txt mapped/* 
+featureCounts -a ../data_in/ddis/sRNAs_ddis_mature.gff -T 8 -O -M --fraction --fracOverlap 1  --fracOverlapFeature 0.9 -t miRNA-3p -g Name -o ../data_in/miRNA_3p_counts.txt mapped/* 
